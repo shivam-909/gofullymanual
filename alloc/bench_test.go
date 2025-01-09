4ab22192ec3acd60f2578ca1fa27ed53ff405b99
shivam <86538330+shivam-909@users.noreply.github.com>
2025-01-09 16:24:10 +0000

    update benchmarks

--- alloc/bench_test.go
+++ alloc/bench_test.go
@@ -1,91 +1,48 @@
 package alloc
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
 
-const N = 1000
-
 func fuzz(n int) int {
 	n = n - 1
-
 	r := rand.Int()
 	if r%2 == 0 {
 		return n + 1
 	}
-
 	n = n - 1
-
 	return n + 2
 }
 
-func BenchmarkLargeAllocs(b *testing.B) {
-	b.Run("CustomAllocator", func(b *testing.B) {
-		p := Allocate[int](8)
+func BenchmarkAllocs(b *testing.B) {
+	rand.Seed(42)
+	allocationSizes := []int{256, 5120, 10000}
+	NValues := []int{1000, 10000, 100000}
 
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < N; j++ {
-				slice := AllocateSlice[int](fuzz(10000))
-				slice[0] = 1
-				FreeSlice(slice)
-			}
-		}
-		Free(p)
-	})
+	for _, size := range allocationSizes {
+		for _, N := range NValues {
+			b.Run(fmt.Sprintf("CustomAllocator_Size%d_N%d", size, N), func(b *testing.B) {
+				p := Allocate[int](9)
+				for i := 0; i < b.N; i++ {
+					for j := 0; j < N; j++ {
+						slice := AllocateSlice[int](fuzz(size))
+						slice[0] = 1
+						FreeSlice(slice)
+					}
+				}
+				Free(p)
+			})
 
-	b.Run("StandardAllocator", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < N; j++ {
-				slice := make([]int, fuzz(10000))
-				slice[0] = 1
-			}
+			b.Run(fmt.Sprintf("StandardAllocator_Size%d_N%d", size, N), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					for j := 0; j < N; j++ {
+						slice := make([]int, fuzz(size))
+						slice[0] = 1
+					}
+				}
+			})
 		}
-	})
-}
-
-func BenchmarkSmallAllocs(b *testing.B) {
-	b.Run("CustomAllocator", func(b *testing.B) {
-		p := Allocate[int](8)
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < N; j++ {
-				slice := AllocateSlice[int](fuzz(5120))
-				slice[0] = 1
-				FreeSlice(slice)
-			}
-		}
-		Free(p)
-	})
-
-	b.Run("StandardAllocator", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < N; j++ {
-				slice := make([]int, fuzz(5120))
-				slice[0] = 1
-			}
-		}
-	})
-}
-
-func BenchmarkTinyAllocs(b *testing.B) {
-	b.Run("CustomAllocator", func(b *testing.B) {
-		p := Allocate[int](8)
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < N; j++ {
-				slice := AllocateSlice[int](fuzz(256))
-				slice[0] = 1
-				FreeSlice(slice)
-			}
-		}
-		Free(p)
-	})
-
-	b.Run("StandardAllocator", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			for j := 0; j < N; j++ {
-				slice := make([]int, fuzz(256))
-				slice[0] = 1
-			}
-		}
-	})
+	}
 }
